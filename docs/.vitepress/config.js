1a46d8f0dea737b22412f4d2a79feda66139fc0b
ronneyhuang <ronneyhuang@wesure.cn>
2022-09-27 14:37:10 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,16 @@
+export default {
+    title: '小浪同学',
+    description: '小浪同学的博客',
+    author: '小浪同学',
+    base: '/',
+    markdown: {
+        lineNumbers: true
+    },
+    lastUpdated: true,
+    themeConfig: {
+        footer: {
+            message: 'Copyright © 2022-present Ronney Wong',
+            copyright: `<a href='https://beian.miit.gov.cn/#/Integrated/index'>粤ICP备2021161765号</a>`
+        }
+    }
+}
